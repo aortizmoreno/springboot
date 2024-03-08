c8eb3451a41af9b03f02468c173289bb85a57962
Alberto <aortizmoreno@deloitte.com>
2024-03-08 09:13:24 -0600

    new changes

---
+++ example1/src/main/java/com/test/example1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.test.example1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String helloWold(){
+        return "Hello World";
+    }
+}

6057f19e4bcd468cc8285902062db90dac2959be
Alberto Ortiz <aortizmoreno@deloitte.com>
2024-07-12 09:53:38 -0600

    new changes

--- example1/src/main/java/com/test/example1/Example1Application.java
+++ example1/src/main/java/com/test/example1/Example1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Example1Application {
 
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(Example1Application.class, args);
 	}
 
